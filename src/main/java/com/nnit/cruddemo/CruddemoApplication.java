713667b826c608dee34e81e1089a5b813224df95
KOSIT AROMSAVA <kosit.arom@gmail.com>
2023-12-12 15:03:53 +0700

    init

---
+++ src/main/java/com/nnit/cruddemo/CruddemoApplication.java
@@ -0,0 +1,39 @@
+package com.nnit.cruddemo;
+
+import com.nnit.cruddemo.dao.StudentDAO;
+import com.nnit.cruddemo.entity.Student;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+		return runner ->{
+			//System.out.println("Hello World");
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Kosit","Aromsava","nong@nn.it.com");
+
+		//save the student object
+		System.out.println("Saving the student");
+		studentDAO.save(tempStudent);
+
+
+		//display id of the saved student
+		System.out.println("Saved student. Generated id:"+tempStudent.getId());
+
+	}
+}
---
+++ src/main/java/com/nnit/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,7 @@
+package com.nnit.cruddemo.dao;
+
+import com.nnit.cruddemo.entity.Student;
+
+public interface StudentDAO {
+    void save(Student theStudent);
+}
